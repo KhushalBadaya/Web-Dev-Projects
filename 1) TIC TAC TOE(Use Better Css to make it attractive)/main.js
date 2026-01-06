35a728242a79ff8676ee834f7c228fe712898dc4
Khushal <Khushal.badaya@gmail.com>
2026-01-06 22:48:40 +0530

    Tic Tack Toe

---
+++ 1) TIC TAC TOE(Use Better Css to make it attractive)/main.js
@@ -0,0 +1,106 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let newbtn = document.querySelector("#newgame");
+let msg =document.querySelector("#winner");
+let msgContainer=document.querySelector(".msg");
+let turnO = true; //playerX,playerO
+let count =0;
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+const resetgame = () =>{
+    turnO=true;
+    count =0;
+   enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+const newGame = () =>{
+    turnO=true;
+    count =0;
+    msgContainer.classList.add("hide");
+    enableBoxes();
+}
+
+
+
+boxes.forEach((box) =>{
+ box.addEventListener("click",()=>{
+    console.log("Box Was Clicked");
+    if(turnO===true){
+       box.innerText = "O";
+        turnO=false;
+    }
+    else{
+        box.innerText="X"
+        turnO=true;
+    }
+    box.disabled=true;
+    count++;
+
+    let isWinner =checkWinner();
+    if(count===9 && !isWinner){
+        gamedraw();
+    }
+
+ }); 
+});
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+
+};
+const checkWinner = () =>{
+for(let pattern of winPatterns){
+
+    // console.log(pattern[0],pattern[1],pattern[2]);
+    // console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
+    let pos1Val=boxes[pattern[0]].innerText;
+    let pos2Val=boxes[pattern[1]].innerText;
+    let pos3Val=boxes[pattern[2]].innerText;
+if(pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
+    if(pos1Val===pos2Val&&pos2Val===pos3Val){
+        showWinner(pos1Val);
+        return true;
+        
+    }
+  }        
+ }
+ return false;
+};
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const gamedraw = ()=>{
+   msg.innerText ="Game was a draw.";
+   msgContainer.classList.remove("hide");
+   disableBoxes();
+
+};
+
+
+
+
+
+
+reset.addEventListener("click",resetgame);
+newbtn.addEventListener("click",newGame);
